f50970b32fcf56f54bfad338c25fcf2b9a0d472b
Andrey Kapskiy <akapsky@yandex.by>
2025-09-15 11:09:58 +0300

    hometask8-1

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const startState = [...state]
+            if (action.payload === 'up') {
+                startState.sort((a, b) => a.name.localeCompare(b.name));
+            } else {
+                startState.sort((a, b) => b.name.localeCompare(a.name));
+            }
+            return startState // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload) // need to fix
         }
         default:
             return state
