eb7e7b36a4808210cdae15efabdbefb62478ff27
Andrey Kapskiy <akapsky@yandex.by>
2025-10-23 11:15:45 +0300

    homework10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,10 @@
 import s from './Loader.module.css'
+import loaderImage from './Loading.png'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+
+        <div className={s.loader}>
+            <img src={loaderImage} alt="Loading..." />
+        </div>
+
+    )
